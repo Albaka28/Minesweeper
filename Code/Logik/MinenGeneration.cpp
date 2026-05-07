9f9ef32de65bb69c7a55bb21bec1bd51d2b186f8
y0119447 <m.starke@tu-bs.de>
2026-05-07 21:25:19 +0200

    für dich ohne <>

--- Code/Logik/MinenGeneration.cpp
+++ Code/Logik/MinenGeneration.cpp
@@ -1,9 +1,16 @@
 #include <random>
-#include <MinenGeneration.h>
+#include "MinenGeneration.h"
 
 using namespace std;
 
+
 void placeMinen(int größe_x, int größe_y, int schwierigkeit){
+
+    int MinenListe[100];
+
+    for (int i = 0; i < 100; i++){
+        MinenListe[i]++;
+    }
 
     // Zufallszeug NICHT ANFASSEN!
     random_device rd;
@@ -12,11 +19,14 @@ void placeMinen(int größe_x, int größe_y, int schwierigkeit){
 
     int feld[größe_x][größe_y];
     int counter = 0;
+    int tmp = 0;
 
     // Füllt 2D-Array mit Zahlen 1-100
     for (int i = 0; i < größe_x; i++){
         for (int j = 0; j < größe_y; j++){
-            feld[i][j] = dist(gen);
+            tmp = dist(gen);
+            feld[i][j] = tmp;
+            MinenListe[tmp-1]++;
         }
     }
     //Platziert Minen je nach Schwierigkeit
